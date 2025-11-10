a84765386ccd07eb9b00ac8619024dfed6de9480
Luciano Leone <luciano.leone@gmail.com>
2025-11-10 14:57:45 -0300

    version final con comentarios

--- pisoplanta/urls.py
+++ pisoplanta/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from pisoplanta.views import home_pisoplanta, crear_centro, listar_centros
+from pisoplanta.views import home_pisoplanta, crear_centro, listar_centros # Importar las vistas desde pisoplanta.views
+#Creamos un archivo URL para la app pisoplanta, como no tenemos otra la dejamos definida en el home del proyecto
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_pisoplanta),
-    path('crear-centro/<nombre>/<operacion>/<activo>/', crear_centro),
-    path('listar-centros/', listar_centros),
+    path('', home_pisoplanta), #Pantalla de inicio de la appo y por lo tanto del proyecto
+    path('crear-centro/<nombre>/<operacion>/<activo>/', crear_centro), # Ruta para crear un centro de trabajo nuevo
+    path('listar-centros/', listar_centros), # Ruta para listar los centros de trabajo de la base de datos
 ]
--- pisoplanta/views.py
+++ pisoplanta/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
-from pisoplanta.models import Centro
+from pisoplanta.models import Centro #importamos el modelo
 # Create your views here.
+
+#Vista del inicio de nuestra app
 def home_pisoplanta(request):
     return render(request, 'homepisoplanta.html')
-
+#vista para crear un centro de trabajo, los campos los recibimos por la URL: nombre, operacion que realiza y si esta activo o no (booleano  True/False)
 def crear_centro(request, nombre, operacion, activo):
     centro = Centro(nombre=nombre, operacion=operacion, activo=activo)
     centro.save()
     return render(request, 'crear_centro.html', {'centro': centro})
-
+#vista para listar los centros de trabajo almacenados en la base de datos
 def listar_centros(request):
     centros = Centro.objects.all()
     return render(request, 'listar_centros.html', {'centros': centros})
